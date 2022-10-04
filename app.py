d2bfb7838c5a682731bf2e6937b5bc568b6ebaf3
Leonardo Ramos <leosaor@outlook.com>
2022-10-04 16:57:27 -0300

    Updates script to accept Portuguese language and GUI Scale in 3

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 import pyautogui as gui
 import time
 
+def doIt():
+    gui.click(button='right')
+    print('FISH GET!')
+    time.sleep(3)
+    gui.click(button='right')
+
 def autofish():
     while True:
-        if gui.locateOnScreen('tofind.png', confidence=.7):
-            gui.click(button='right')
-            print('FISH GET!')
-            time.sleep(3)
-            gui.click(button='right')
+        if gui.locateOnScreen('./images/toFindPtbrGui2.png', confidence=.9):
+            doIt()
+        elif gui.locateOnScreen('./images/toFindPtbrGui3.png', confidence=.9):
+            doIt()
+        elif gui.locateOnScreen('./images/toFindEnGui2.png', confidence=.9):
+            doIt()
+        elif gui.locateOnScreen('./images/toFindEnGui3.png', confidence=.9):
+            doIt()
 
 if __name__ == '__main__':
     print('STARTING...')
