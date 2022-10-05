e72c37d50421ccc10545f0c24fcc5d47d70f19e4
Leonardo Ramos <leosaor@outlook.com>
2022-10-05 16:27:04 -0300

    Checks if the Fishing Rod is in the water

--- app.py
+++ app.py
@@ -3,21 +3,34 @@ import time
 
 def doIt():
     gui.click(button='right')
-    print('FISH GET!')
-    time.sleep(3)
+    time.sleep(2.45)
     gui.click(button='right')
 
+def resetFishingRod():
+    print('FISHING ROD IS NOT IN THE WATER, RESTARTING...')
+    gui.scroll(1000)
+    time.sleep(0.5)
+    gui.scroll(-1000)
+    time.sleep(0.5)
+    gui.click(button='right')
+    autofish()
+
 def autofish():
-    while True:
+    while gui.locateOnScreen('./images/respingos.png', grayscale=True, confidence=0.4):
         if gui.locateOnScreen('./images/toFindPtbrGui2.png', confidence=.9):
             doIt()
+            print('FISH CAUGHT')
         elif gui.locateOnScreen('./images/toFindPtbrGui3.png', confidence=.9):
             doIt()
+            print('FISH CAUGHT')
         elif gui.locateOnScreen('./images/toFindEnGui2.png', confidence=.9):
             doIt()
+            print('FISH CAUGHT')
         elif gui.locateOnScreen('./images/toFindEnGui3.png', confidence=.9):
             doIt()
-
+            print('FISH CAUGHT')
+    resetFishingRod()
+    
 if __name__ == '__main__':
     print('STARTING...')
     time.sleep(5)
